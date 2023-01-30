ae43f0f182159582ba6fafda4b4a857622910bd4
Hong Gu <macrogu@qq.com>
2023-01-30 17:56:35 +0800

    init

---
+++ Source/FileDownloaderDemo/FileDownloaderDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FileDownloaderDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FILEDOWNLOADERDEMO_API AFileDownloaderDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
